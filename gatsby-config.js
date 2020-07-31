9b5a942ae231588c3b78748a2fda1ce2587c7b19
Paul Applegate <paul@theapplegates.com>
2020-07-30 21:10:18 -0400

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {
+        preset: `theme-ui-sketchy-preset`,
+        prismPreset: `oceanic-next`,
+        webfontURL:
+          "https://fonts.googleapis.com/css?family=Architects+Daughter",
+},
+    },
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `Living Life`,
+    author: `Paul Applegate`,
+    description: `Follow my daily hijinks here <br>
+& occasionally a serious subject like cancer. Fuck cancer.`,
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/papplegate`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/theapplegates`,
+      },
+    ],
+  },
+}
